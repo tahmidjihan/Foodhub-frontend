44c87177f58089f722d47ddf60f67e71eee3987d
Tahmid Jihan <tahmidjihan321@gmail.com>
2026-02-03 03:28:30 +0600

    cookies

--- app/auth/auth.ts
+++ app/auth/auth.ts
@@ -1,6 +1,10 @@
 import { createAuthClient } from 'better-auth/react';
+
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'https://food-backend-rust-omega.vercel.app',
+  baseURL: typeof window !== 'undefined' ? window.location.origin : '',
+  fetchOptions: {
+    credentials: 'include',
+  },
 });
-export const { useSession } = createAuthClient();
+
+export const { useSession } = authClient;
